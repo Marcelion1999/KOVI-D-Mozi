079fecc2737ab4985126518824cb7929866127db
Marcell Dániel Szabó <OB4C3L@student.uni-pannon.hu>
2020-11-23 19:51:02 +0100

    First Commit

---
+++ KOVI-D Mozi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KOVI_D_Mozi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
